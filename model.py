de6170697b5f4431d0940f9f0f8df9f0804be65e
Anderson David Patiño Caicedo <andersondavidpc@ufps.edu.co>
2022-12-14 23:08:28 -0500

    Importada la clase LinearRegression de la librería sklearn o scikit-learn con ejemplo de su implementación

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 # Cargar los datos del archivo data.csv en un marco de datos de Pandas
 df = pd.read_csv('data.csv', sep=';')
@@ -17,3 +18,13 @@ mes = df['mes']
 
 # Acceder a la columna año del marco de datos
 anio = df['anio']
+
+
+# Crear un modelo de regresión lineal
+model = LinearRegression()
+
+# Entrenar el modelo con los datos de df
+model.fit(df[["columna_1", "columna_2", ...]], df["columna_objetivo"])
+
+# Hacer predicciones con el modelo entrenado
+predicciones = model.predict(df[["columna_1", "columna_2", ...]])
